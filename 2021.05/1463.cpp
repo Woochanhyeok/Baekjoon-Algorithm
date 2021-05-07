a4202109758599a0ed9f804013daf9f2f0bd970b
Woo <dncksgur1@naver.com>
2021-05-08 00:03:09 +0900

    2021.05.07

---
+++ 2021.05/1463.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    int X; cin >> X;
+
+    int DP[1000001] = {0,};
+
+    for (int i = 2; i <= X; i++) {
+        DP[i] = DP[i - 1] + 1;
+
+        if (i % 3 == 0) {
+            DP[i] = min(DP[i], DP[i / 3] + 1);
+        }
+        if (i % 2 == 0) {
+            DP[i] = min(DP[i], DP[i / 2] + 1);
+        }
+    }
+    cout << DP[X];
+    
+}
