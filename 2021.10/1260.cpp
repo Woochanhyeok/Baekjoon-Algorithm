3afa955ec91b71e0ed5cf0eb94bad8304ac69d89
Woo <dncksgur1@naver.com>
2021-10-01 23:02:10 +0900

    2021.10.01

---
+++ 2021.10/1260.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <queue>
+#include <stack>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool visited[1001];
+vector<vector<int>> vec(1001);
+
+void dfs(int node) {
+	visited[node] = true;
+	cout << node << " ";
+	sort(vec[node].begin(), vec[node].end());
+	for (int i = 0; i < vec[node].size(); i++) {
+		int next = vec[node][i];
+		if (!visited[next]) {
+			dfs(next);
+		}
+	}
+}
+
+int main()
+{
+	int N, M, V; cin >> N >> M >> V;
+
+	for (int i = 0; i < M; i++) {
+		int a, b; cin >> a >> b;
+		vec[a].push_back(b);
+		vec[b].push_back(a);
+	}
+	
+
+	//dfs
+	dfs(V);
+	cout << endl;
+	//bfs
+	fill_n(visited, 1001, false);
+	queue<int> q;
+	q.push(V);
+	visited[V] = true;
+	while (!q.empty()) {
+		int node = q.front();
+		q.pop();
+		cout << node << " ";
+		sort(vec[node].begin(), vec[node].end());
+		for (int i = 0; i < vec[node].size(); i++) {
+			int next = vec[node][i];
+			if (!visited[next]) {
+				q.push(next);
+				visited[next] = true;
+			}
+		}
+	}
+
+}
