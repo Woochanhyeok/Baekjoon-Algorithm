88db861d7ca8e06fcd8222045ab5eebb1bd6f8f6
Woo <dncksgur1@naver.com>
2021-10-26 22:06:24 +0900

    2021.10.26

---
+++ 2021.10/1966.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+//현재 맨앞의 것보다 중요도가 더 큰게 있는지 확인 => 더 큰게 있으면 false
+bool check(queue<pair<int, int>> q) {
+	bool result = true;
+	pair<int, int> first = q.front(); q.pop();
+	int count = q.size();
+	for (int i = 0; i < count; i++) {
+		if (first.second < q.front().second) {
+			result = false;
+			break;
+		}
+		q.pop();
+	}
+	return result;
+}
+
+int main()
+{
+	int T; cin >> T;
+	vector<int> answer;
+	for (int i = 0; i < T; i++) {
+		int N, M; cin >> N >> M;
+		queue<pair<int, int>> q;
+		vector<int> v;
+		for (int i = 0; i < N; i++) {
+			int num = 0; cin >> num;
+			q.push({ i,num });
+		}
+		
+		while (v.size() != N) {
+			if (check(q)) {
+				v.push_back(q.front().first);
+				q.pop();
+			}
+			else {
+				pair<int, int> temp = q.front();
+				q.pop();
+				q.push(temp);
+			}
+		}
+
+		for (int i = 0; i < N; i++) {
+			if (v[i] == M)
+				answer.push_back(i+1);
+		}
+	}
+
+	for (int i = 0; i < answer.size(); i++) {
+		cout << answer[i] << "\n";
+	}
+}
